1d493bcc43be720bc27d3be47341da5076400c4c
OKYSTA <c0119059e1@edu.teu.ac.jp>
2022-08-30 03:13:11 +0900

    versionup?

--- plane_detection.py
+++ plane_detection.py
@@ -3,25 +3,30 @@
 
 from collections import deque
 import math
-from platform import node
 import cv2
 import numpy as np
+import scipy.io
+import random
+import sys
 import pyrealsense2 as rs
 from sklearn.decomposition import PCA
 from sklearn.metrics import mean_squared_error
 
-# 面番号
-NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Reject理由
+MISSING_DATA = 0
+BIG_DIFF = 1
+BIG_MSE = 2
 
-# 処理の概形
+REAL_TIME = False
+
+# 処理の概形\
 # 入力は2次元になった
-def fast_plane_extraction(depth_image):
+def fast_plane_extraction(verts):
     # データ構造構築(ステップ1)
-    nodes, edges = init_graph(depth_image)
-    print(f"num of edges: {len(edges)}")
+    nodes, edges = init_graph(verts)
 
-    # # 粗い平面検出(ステップ2)
-    # boudaries, pai = ahcluster(nodes, edges)
+    # 粗い平面検出(ステップ2)
+    boudaries, pai = ahcluster(nodes, edges)
     # # 粗い平面検出を精緻化(ステップ3)
     # cluster, pro_pai = refine(boudaries, pai)
     # return cluster, pro_pai
@@ -30,76 +35,123 @@ def fast_plane_extraction(depth_image):
 # グループのクラス
 class Node:
     def __init__(self, data):
-        self.data = data    # np.array型の深さの集合(10×10)
+        self.data = np.nan_to_num(data)    # np.array型の深さの集合(10×10)
         self.g_num = 0 # グループの番号,初期値0
-        self.rejected = False #reject_nodeに当てはまったかどうか
-        self.center = np.average(data)
-        self.t_mse = (1.6e-6*self.center**2+5)**2
+        self.reject_type = None #Rejectされた理由　Noneの場合はされていない
+        self.compute()
+        
+        # self.t_mse = (1.6e-6*self.center[2]**2+5)**2
 
         # 上下左右のノード
         self.left = None
         self.right = None
         self.up = None
         self.down = None
+        self.edge = []
+
+    def compute(self):
+        self.center = [np.average(self.data[..., 0]), np.average(self.data[..., 1]), np.average(self.data[..., 2])]    # 平均(重心)
+        self.cov = np.cov(self.data[..., :2].reshape(self.data.size//3, 2).T, self.data[..., 2].reshape(self.data.size//3,), rowvar=True)     # 分散共分散行列
+
+        eig = np.linalg.eig(self.cov)   
+        ind = np.argmin(eig[0])
+        self.mse = eig[0][ind] * self.data.size    # 最小の固有値
+
+        if eig[1][ind,0]*self.center[0] + eig[1][ind,1]*self.center[1] + eig[1][ind,2]*self.center[2] <= 0:
+            self.normal = eig[1][ind]  #固有ベクトル(法線)
+        else:
+            self.normal = -eig[1][ind]
+    
+    def push(self, node, success=False):
+        self.prev_data = np.copy(self.data)
+        self.data = np.concatenate([self.data, node.data])
+        self.compute()
+        if success:
+            self.edge.remove(node)
+            self.edge += node.edge
+            node.g_num = self.g_num
+
+    def undo(self):
+        self.data = np.copy(self.prev_data)
+        # self.compute()
 
 # データ構造構築
-def init_graph(depth_image, h=10, w=10):
+def init_graph(verts, h=10, w=10):
+    global NUMBERS
     nodes = []
     rejected_nodes = []
     edges = []
+        
+    NUMBERS = list(range(1,10000))    # 面番号
 
     # 10×10が横にいくつあるかの数
-    width = len(depth_image[0]) // w 
-    height = len(depth_image) // h
+    width = len(verts[0]) // w 
+    height = len(verts) // h
 
     for i in range(height):
         nodes_line = []
         for j in range(width):
             # node は論文の v
-            node = Node(depth_image[i*h:i*h+h,j*w:j*w+w])
+            node = Node(verts[i*h:i*h+h,j*w:j*w+w])
             # nodeの除去の判定
             if reject_node(node):
-                node.rejected = True
                 rejected_nodes.append(node)
             nodes_line.append(node)
         nodes.append(nodes_line)
 
-    # print(f"rejectされていないノード数: {sum(len(v) for v in nodes) - len(rejected_nodes)}")
-    # rand = random.randint(0, len(rejected_nodes))
-    # print(f"rejectされたノードの例: {[node.data for node in rejected_nodes[rand:rand+1]]}")
-    # print("-------------------------------------------------------------------------------")
-
     # 右・下の繋がりを調べる
     for i in range(len(nodes)):
         for j in range(len(nodes[0])):
-            if not nodes[i][j].rejected:
-                if j+1<len(nodes[0]) and not rejectedge(nodes[i][j], nodes[i][j+1]): #書き方不安
+            if nodes[i][j].reject_type == None:
+                if j+1<len(nodes[0]) and not rejectedge(nodes[i][j], nodes[i][j+1]):
                     nodes[i][j].right = nodes[i][j+1]
+                    nodes[i][j].edge.append(nodes[i][j+1])
+
                     nodes[i][j+1].left = nodes[i][j]
+                    nodes[i][j+1].edge.append(nodes[i][j])
+
                     edges.append(nodes[i][j])
                 if i+1<len(nodes) and not rejectedge(nodes[i][j], nodes[i+1][j]):
                     nodes[i][j].down = nodes[i+1][j]
+                    nodes[i][j].edge.append(nodes[i+1][j])
+
                     nodes[i+1][j].up = nodes[i][j]
+                    nodes[i+1][j].edge.append(nodes[i][j])
+
                     edges.append(nodes[i][j])
 
-    return nodes, list(set(edges))
+    edges = list(set(edges))
+    
+    print(f"rejectされていないノード数: {sum(len(v) for v in nodes) - len(rejected_nodes)}")
+    print(f"エッジ数: {len(edges)}")
+    rand = random.randint(0, len(rejected_nodes))
+    # print(f"rejectされたノードの例: {[node.data for node in rejected_nodes[rand:rand+1]]}")
+    # print(f"reject理由: {[node.reject_type for node in rejected_nodes[rand:rand+1]]}")
+    # print("-------------------------------------------------------------------------------")
+
+    return nodes, edges
 
 # ノードの除去
-def reject_node(node, threshold=999):
-    data = node.data.astype(np.int32)
+def reject_node(node, threshold=100):
+    data = node.data
+
     # データが欠落していたら
-    if np.any(data == 0):
+    if np.any(data[..., -1] == 0):
+        node.reject_type = MISSING_DATA
         return True
 
     # 周囲4点との差
-    v_diff = np.abs(np.diff(data, axis=0))  #縦方向の差
-    h_diff = np.abs(np.diff(data))          #横方向の差
+    v_diff = np.linalg.norm(data[:-1] - data[1:], axis=2)
+    h_diff = np.linalg.norm(data[:, :-1] - data[:, 1:], axis=2)
 
     if v_diff.max() > threshold or h_diff.max() > threshold:
+        node.reject_type = BIG_DIFF
         return True
 
-    # まだ決めていない
-    if mse(data) > node.t_mse:
+    # print(f"mse = {node.mse}    t = {node.t_mse}")
+
+    if node.mse > 2000:   #閾値適当
+        node.reject_type = BIG_MSE
         return True
 
     return False
@@ -107,19 +159,10 @@ def reject_node(node, threshold=999):
 # 連結関係の除去
 def rejectedge(node1, node2):
     # 欠落していなければ
-    if np.any(node2.data == 0):
+    if node2.reject_type != None:
         return True 
 
-    normals = []
-
-    for data in [node1.data, node2.data]:
-        eig = np.linalg.eig(data)   # [0]: 固有値 shape(10, )      [1]: 固有ベクトル shape(10, 10)
-        normals.append(eig[1][:, np.argmin(eig[0])])
-
-    # pfn = np.dot(normals[0], normals[1])
-    pfn = np.abs(np.sum(normals[0]*normals[1]))
-    # print(f"pfn: {eig[0]}")
-    # print("-----------------------------------------------------------")
+    pfn = np.abs(np.dot(node1.normal, node2.normal))
 
     if  pfn <= 0.7:
         return True
@@ -141,16 +184,18 @@ def mse(array):
 def ahcluster(nodes, edges):
 
     # MSEの昇順のヒープを作る
-    # queue = build_mse_heap(nodes)
-    queue = edges
-    boudaries = np.array()
-    pai = np.array()
-    suf_choice = None
-    suf_best = np.zeros(1)
+    queue = build_mse_heap(edges)
+    boudaries = np.array([])
+    pai = np.array([])
 
     # queueの中身がある限り
     while queue != []:
-        suf = popmin(queue)
+        suf = queue.pop()
+
+        # 周りが同じ面ならばみない
+        if (suf.left and suf.g_num == suf.left.g_num) and (suf.right and suf.g_num == suf.right.g_num) and (suf.up and suf.g_num == suf.up.g_num) and (suf.down and suf.g_num == suf.down.g_num):
+            continue
+
         # vがマージされているならば
         """
         if suf not in nodes:
@@ -161,71 +206,27 @@ def ahcluster(nodes, edges):
         if suf.g_num == 0:
             suf.g_num = NUMBERS.pop()
 
+        # vと連結関係にあるuを取り出して
         # 連結関係のノードをマージする
-        try:
-            suf_test = np.hstack[suf.left.data, suf.data]
-        except:
-            pass
-
-        # 一番MSEが小さいノードを選ぶ
-        if mse(suf_test) < mse(suf_best):
-            # 連結しているノードの中で一番優秀なものをbest,くっつけた状態のものをchoiceへ暫定的に
-            if suf.left.g_num == suf.g_num:
-                pass
-            else:
-                suf_best = suf_test
-                suf_choice = suf.left
-
-        try:
-            suf_test = np.hstack[suf.data, suf.right.data]
-        except:
-            pass
-
-        if mse(suf_test) < mse(suf_best):
-            # 連結しているノードの中で一番優秀なものをbest,くっつけた状態のものをchoiceへ暫定的に
-            if suf.right.g_num == suf.g_num:
-                pass
-            else:
-                suf_best = suf_test
-                suf_choice = suf.right
-
-        try:
-            suf_test = np.vstack[suf.up.data, suf.data]
-        except:
-            pass
-
-        if mse(suf_test) < mse(suf_best):
-            # 連結しているノードの中で一番優秀なものをbest,くっつけた状態のものをchoiceへ暫定的に
-            if suf.up.g_num == suf.g_num:
-                pass
-            else:
-                suf_best = suf_test
-                suf_choice = suf.up
-        
-        try:
-            suf_test = np.vstack[suf.data, suf.down.data]
-        except:
-            pass
-
-        if mse(suf_test) < mse(suf_best):
-            # 連結しているノードの中で一番優秀なものをbest,くっつけた状態のものをchoiceへ暫定的に
-            if suf.down.g_num == suf.g_num:
-                pass
-            else:
-                suf_best = suf_test
-                suf_choice = suf.down
+        best_mse = [math.inf, None]
+        for edg in suf.edge:
+            suf.push(edg)
+            if suf.mse < best_mse[0]:
+                best_mse = [suf.mse, edg]
+            suf.undo()
 
         # マージ失敗
-        if mse(suf_best) >= 999999:
-            #boudaries = boudaries.append(suf)
+        if best_mse[0] >= 0.001:
+            continue
+            boudaries = boudaries.append(suf)
             pai = pai.append(plane(suf_best))
 
         # マージ成功
         else:
-            if not suf_choice != None:
-                queue.append(suf)
-                suf_choice.g_num = suf.g_num
-    return pai
+            suf.push(best_mse[1], True)
+            queue.append(suf)
+            # suf_choice.g_num = suf.g_num
+    return boudaries, pai
 
 # 平面近似PCA
 def plane(v):
@@ -233,10 +234,12 @@ def plane(v):
     return pca.fit(v)
 
 # ヒープの作成
-def build_mse_heap(nodes):
-    # 1つずつmseを計算し直して並べ替える
-    # クラスのメンバーとしてmseの値が必要？
-    queue = sorted(nodes, key=mse)
+def build_mse_heap(edges):
+    # 1つずつmseを計算し直して並べ替える)
+    queue = sorted(edges, key=lambda node: node.mse)
+    print(queue[len(queue)//2].mse)
+    print(queue[10].mse)
+    print(queue[-1].mse)
     return queue
 
 # 先頭を取り出す
@@ -245,32 +248,29 @@ def popmin(queue):
     return choice
 
 # 粗い平面検出の精緻化
-def refine(edges, pai):
+# Bk,Rk,Rlは任意の順番のという意味かもしれない
+def refine(boundaries, pai):
+    # キュー
     queue = deque()
     refine = np.array()
     rf_nodes = np.array()
     rf_edges = np.array()
-    suf_num = 0
-
-    for node in edges:
-        suf_num = node.g_num
-        # 何らかの面に属しているとき
-        if not suf_num == 0:
-            # 上下左右のノードと番号が違ったら境界
-            if (node.left == None) or (node.right == None) or (node.up == None) or (node.down == None):
-                queue.append(node)
-                continue
-            elif (suf_num != node.left.g_num) or (suf_num != node.right.g_num) or (suf_num != node.up.g_num) or (suf_num != node.down.g_num):
-                queue.append(node)
-
-    while queue != None:
-        points = queue.popleft()
-        for columns in points:
-            for i in range(10):
-                if math.dist(columns[i], pai) >= 9 + mse()
-
-    """
-    # 遺物
+    for k, boundary in enumerate(boundaries):
+        refine_k = np.array()
+        # 謎ポイント
+        refine = refine.append(refine_k)
+        # フチを除く
+        for v in boundary:
+            # 上下左右のノードが面内ではないならば
+            if v not in boundary:
+                # 境界点判定
+                boundary = boundary.remove(v)
+        # 除去した境界のポイントを個別でみる
+        for p in v:
+            # kとは何の値？→インデックス？pとのタプルで追加しろといっている？
+            queue.append({p, k})
+        if boundary != None:
+            rf_nodes = rf_nodes.appned(boundary)
     while queue != None:
         points = queue.popleft()
         k = points[1]
@@ -291,82 +291,116 @@ def refine(edges, pai):
         boundary_k = boundary_k.append(refine_k)
     cluster, pro_pai = ahcluster(rf_nodes, rf_edges)
     return cluster, pro_pai
-    """
-    
-    # 仮
-    return pro_pai
 
 def visualization(color_image, nodes):
-    color_image[::10] = [0, 0, 0]
-    color_image[:, ::10] = [0, 0, 0]
+    color_image = np.copy(color_image)
+    BLACK = [0, 0, 0]
+    RED = [0, 0, 255]
+    ORANGE = [17, 144, 255]
+    PURPLE = [255, 0, 127]
+    PINK = [102, 0, 255]
+    GRAY = [128, 128, 128]
+    GREEN = [51, 255, 51]
+    BLUE = [255, 90, 0]
+
+    color_image[::10] = BLACK
+    color_image[:, ::10] = BLACK    
     
     for h in range(0, len(color_image), 10):
         for w in range(0, len(color_image[0]), 10):
-            if nodes[h//10][w//10].rejected:
-                color_image[h+4:h+7, w+4:w+7] = [0, 0, 255]
+            if nodes[h//10][w//10].reject_type == MISSING_DATA:
+                color_image[h+4:h+7, w+4:w+7] = BLACK
+            elif nodes[h//10][w//10].reject_type == BIG_DIFF:
+                color_image[h+4:h+7, w+4:w+7] = ORANGE
+            elif nodes[h//10][w//10].reject_type == BIG_MSE:
+                color_image[h+4:h+7, w+4:w+7] = RED
 
             if nodes[h//10][w//10].up:
-                color_image[h-5:h+5, w+5] = [102, 0, 255]
+                color_image[h-5:h+5, w+5] = PURPLE
             if nodes[h//10][w//10].down:
-                color_image[h+5:h+15, w+5] = [102, 0, 255]
+                color_image[h+5:h+15, w+5] = PURPLE
             if nodes[h//10][w//10].left:
-                color_image[h+5, w-5:w+5] = [102, 0, 255]
+                color_image[h+5, w-5:w+5] = PURPLE
             if nodes[h//10][w//10].right:
-                color_image[h+5, w+5:w+15] = [102, 0, 255]
+                color_image[h+5, w+5:w+15] = PURPLE
+
+            # if nodes[h//10][w//10].g_num != 0:
+            #     color_image[h+1:h+10, w+1:w+10] = [nodes[h//10][w//10].g_num+100]
 
     return color_image
 
 if __name__ == '__main__':
-    # Configure depth and color streams
-    pipeline = rs.pipeline()
-    config = rs.config()
+    if not REAL_TIME:
+        data = scipy.io.loadmat('frame.mat')["frame"]
+        verts = data[0, 0][0]
+        color_image = data[0, 0][1]
 
-    # Convert images to numpy arrays
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-    # Start streaming
-    pipeline.start(config)
-    align = rs.align(rs.stream.color)
-
-    while True:
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        frames = align.process(frames)
-
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data()) #計算に使用
-        color_image = np.asanyarray(color_frame.get_data())
-
-        cluster, pro_pai = fast_plane_extraction(depth_image)
+        cluster, pro_pai = fast_plane_extraction(verts)
 
         color_image = visualization(color_image, cluster)
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_scale = cv2.convertScaleAbs(depth_image, alpha=0.03)  #0～255の値にスケール変更している
-
-        depth_colormap = cv2.applyColorMap(depth_scale, cv2.COLORMAP_JET)
-
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
-
-        cv2.imwrite("debug_image.png", images)
-
-        # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        key = cv2.waitKey(1)
-        prop_val = cv2.getWindowProperty('RealSense', cv2.WND_PROP_ASPECT_RATIO)
-
+        cv2.imshow('RealSense', color_image)
+        key = cv2.waitKey(0)
         if key == ord("s"):
-            cv2.imwrite('./out.png', images)
-        if key == ord("q") or (key != -1) or (prop_val < 0):
-            # Stop streaming
-            pipeline.stop()
-            cv2.destroyAllWindows()
-            break
+                cv2.imwrite('./out_nr.png', color_image)
+        cv2.destroyAllWindows()
+    
+    else:
+        # Configure depth and color streams
+        pipeline = rs.pipeline()
+        config = rs.config()
+
+        # Convert images to numpy arrays
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        # Start streaming
+        pipeline.start(config)
+        align = rs.align(rs.stream.color)
+        
+        pc = rs.pointcloud()
+
+        while True:
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            frames = align.process(frames)
+
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data()) #計算に使用
+            color_image = np.asanyarray(color_frame.get_data())
+
+            points = pc.calculate(depth_frame)
+            v = points.get_vertices()
+            verts = np.asanyarray(v).view(np.float32).reshape(480, 640, 3)  # xyz
+
+            cluster, pro_pai = fast_plane_extraction(verts)
+
+            color_image = visualization(color_image, cluster)
+
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_scale = cv2.convertScaleAbs(depth_image, alpha=0.03)  #0～255の値にスケール変更している
+
+            depth_colormap = cv2.applyColorMap(depth_scale, cv2.COLORMAP_JET)
+
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
+
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            key = cv2.waitKey(1)
+            prop_val = cv2.getWindowProperty('RealSense', cv2.WND_PROP_ASPECT_RATIO)
+
+            if key == ord("s"):
+                cv2.imwrite('./out.png', images)
+            if key == ord("q") or (prop_val < 0):
+                # Stop streaming
+                pipeline.stop()
+                cv2.destroyAllWindows()
+                break
